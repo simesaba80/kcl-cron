cafbf2d8cbb10040a492dcc4f03b6cd414fa2d13
simesaba80 <simesaba1120@gmail.com>
2024-11-16 00:36:00 +0900

    postリクエストサンプルの作成

--- task/sendpost.go
+++ task/sendpost.go
@@ -11,19 +11,27 @@ import (
 func UpdateAccessToken() {
 	// ここに定期実行したい処理を書く
 	type reqBody struct {
-		UserID         string `json:"user_id"`
-		MealName       string `json:"meal_name"`
-		CaloriePer100g int    `json:"calorie_per_100g"`
-		Date           string `json:"date"`
+		UserID        string  `json:"user_id"`
+		MealName      string  `json:"meal_name"`
+		Calories      int     `json:"calories"`
+		Protein       float64 `json:"protein"`
+		Fat           float64 `json:"fat"`
+		Carbohydrates float64 `json:"carbohydrates"`
+		Salt          float64 `json:"salt"`
+		Date          string  `json:"date"`
 	}
 	reqBody1 := reqBody{
-		UserID:         "uids893njkdf89",
-		MealName:       "カレー",
-		CaloriePer100g: 100,
-		Date:           "2024-11-15",
+		UserID:        "uids893njkdf89",
+		MealName:      "カレー",
+		Calories:      1000,
+		Protein:       20.0,
+		Fat:           30.0,
+		Carbohydrates: 40.0,
+		Salt:          5.0,
+		Date:          "2024-11-15",
 	}
 	fmt.Println(reqBody1)
-	jsonData, err := json.Marshal(reqBody1)
+	jsonData, _ := json.Marshal(reqBody1)
 	client := &http.Client{}
 	resp, err := client.Post("http://app:8080/meal/create", "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
