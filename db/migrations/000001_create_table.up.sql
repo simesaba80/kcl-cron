18f6008cac42d798f15a273e5a598d29e7f8e9a5
simesaba80 <simesaba1120@gmail.com>
2024-11-17 02:21:33 +0900

    カラムの変更と削除

--- db/migrations/000001_create_table.up.sql
+++ db/migrations/000001_create_table.up.sql
@@ -1,6 +1,6 @@
 create table if not exists users(
 id serial primary key,
-uid varchar(100),
+uid varchar(100) not null unique,
 fitbit_user_id varchar(10),
 name varchar(20) not null,
 sex varchar(5) not null,
@@ -13,14 +13,11 @@ refresh_token varchar(250),
 created_at timestamp default current_timestamp
 );
 
-create table if not exists momentum(
+create table if not exists activities(
 id serial primary key,
 user_id varchar(100) not null,
 steps int,
 calories int,
-distance int,
-max_heart_rate int,
-min_heart_rate int,
 date date not null,
 created_at timestamp default current_timestamp
 );
@@ -34,7 +31,7 @@ light_sleep int,
 rem_sleep int,
 wake int,
 date date not null,
-created_at timestamp default current_timestamp
+created_at timestamp default current_timestamp  
 );
 
 create table if not exists meal(
@@ -48,5 +45,5 @@ carbohydrates real,
 salt real,
 calcium real,
 date date not null,
-created_at timestamp default current_timestamp
+created_at timestamp default current_timestamp  
 );
