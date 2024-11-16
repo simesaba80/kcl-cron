18f59ef41bf69554032f0b2b8073bc22d17c9a94
simesaba80 <simesaba1120@gmail.com>
2024-11-17 00:50:48 +0900

    sleepSummaryの取得

--- task/update_tokentemp.go
+++ task/update_tokentemp.go
@@ -1,20 +1,44 @@
 package task
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/simesaba80/go-cron/db"
 )
 
 func Test() {
 	// ここに定期実行したい処理を書く
 	client := &http.Client{}
-	resp, err := client.Get("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23PRT4&redirect_uri=https://lifecampusu.jp/callback&scope=activity" + "%20" + "heartrate" + "%20" + "sleep")
+	req, err := http.NewRequest("GET", "https://api.fitbit.com/1.2/user/C5N3BG/sleep/date/2024-11-16.json", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1BSVDQiLCJzdWIiOiJDNU4zQkciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJociByc2xlIiwiZXhwIjoxNzMxNzk5ODA5LCJpYXQiOjE3MzE3NzEwMDl9.vkGsFDrCNYvklvZ-Nqh7MyOtec1tV8RBzSOJexF5VWk")
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 	//bodyデータの読み取り
+	type sleepStage struct {
+		Deep  int `json:"deep"`
+		Light int `json:"light"`
+		Rem   int `json:"rem"`
+		Wake  int `json:"wake"`
+	}
+	type sleepSummary struct {
+		Stages             sleepStage `json:"stages"`
+		TotalMinutesAsleep int        `json:"totalMinutesAsleep"`
+		TotalSleepRecords  int        `json:"totalSleepRecords"`
+		TotalTimeInBed     int        `json:"totalTimeInBed"`
+	}
+	type bodySleep struct {
+		Summary sleepSummary `json:"summary"`
+	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
@@ -22,13 +46,28 @@ func Test() {
 	//json形式の[]byteから構造体へパース
 	//構造体のフィールドに`json:"name"`のようにタグをつけることで、jsonのキーと構造体のフィールドを紐付ける(タグがなければフィールド名と同じキーを探す)
 	//`json:"-"のフィールドは無視される`
-	// meal := []db.Meal{}
-	// err = json.Unmarshal(body, &meal)
+	// jsonBytes := []byte(body)
+	// err = json.Unmarshal(body, &jsonBytes)
 	// if err != nil {
 	// 	panic(err)
 	// }
-	// // crud.SaveUsersData(meal)
-	fmt.Println(string(body))
+	// crud.SaveUsersData(meal)
+	responsData := bodySleep{}
+	if err := json.Unmarshal(body, &responsData); err != nil {
+		panic(err)
+	}
+	fmt.Println("body: " + string(body))
+	fmt.Println("responsData: ", responsData)
+	sleep := db.Sleep{
+		UserID:     "C5N3BG",
+		Hours:      responsData.Summary.TotalMinutesAsleep,
+		DeepSleep:  responsData.Summary.Stages.Deep,
+		LightSleep: responsData.Summary.Stages.Light,
+		RemSleep:   responsData.Summary.Stages.Rem,
+		Wake:       responsData.Summary.Stages.Wake,
+		Date:       "2024-11-16",
+	}
+	fmt.Println(sleep)
 	fmt.Println(resp.Status)
 	// fmt.Println(meal)
 }
