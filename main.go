7e9fa6b5888c98fb1730311ec823df70f852d8b0
simesaba80 <simesaba1120@gmail.com>
2024-11-17 04:33:39 +0900

    定期実行タスクの設定

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/go-co-op/gocron"
 	"github.com/simesaba80/go-cron/db"
 	"github.com/simesaba80/go-cron/task"
 	"github.com/simesaba80/go-cron/utils"
@@ -17,35 +19,12 @@ func funcC() {
 	fmt.Println("Ping OK")
 }
 
-// var DB *bun.DB
-// var SqlDB *sql.DB
-
-// func Connectdb() {
-// 	// Bunを使ってDB接続
-// 	dsn := utils.DBURL
-// 	SqlDB = sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-// 	DB = bun.NewDB(SqlDB, pgdialect.New())
-// 	if err := SqlDB.Ping(); err != nil {
-// 		log.Fatalf("Failed to connect to database: %v", err)
-// 		return
-// 	}
-
-// 	// クエリーフックを追加
-// 	DB.AddQueryHook(bundebug.NewQueryHook(
-// 		bundebug.WithVerbose(true),
-// 	))
-// }
-
 func main() {
 	utils.LoadConfig()
 	db.Connectdb()
 	defer db.DB.Close()
-	// task.UpdateAccessToken()
-	// scheduler := gocron.NewScheduler(time.Local)
-	// scheduler.Every(5).Second().Do(funcC)            // 30分に1回
-	// scheduler.Every(15).Minute().Do(task.GetAllUser) // 30秒に1回
+	scheduler := gocron.NewScheduler(time.Local)
+	scheduler.Every(15).Minute().Do(task.SaveActivity)  // 30分に1回
+	scheduler.Every(15).Minute().Do(task.SaveSleepData) // 30秒に1回
 	// scheduler.StartBlocking()
-	// task.SaveSleepData()
-	task.SaveActivity()
-	task.SaveSleepData()
 }
